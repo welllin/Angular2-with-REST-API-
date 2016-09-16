556359ac1f2a3e397fabad0e3457558e0feb1063
Well <wellup639936@gmail.com>
2016-09-16 20:23:46 +1200

    removed dataservice

--- app/app.module.ts
+++ app/app.module.ts
@@ -6,12 +6,10 @@ import { HttpModule} from '@angular/http';
 import { AppComponent }  from './app.component';
 import { CustomersComponent } from './customers/customers.component';
 import { app_routing } from './app.routing';
-import { DataService } from './shared/services/data.service';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule, ReactiveFormsModule, HttpModule, app_routing ],
   declarations: [ AppComponent, CustomersComponent ],
-  providers:    [ DataService ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
